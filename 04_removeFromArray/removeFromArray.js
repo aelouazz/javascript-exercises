58375ef479b89da3f756cbb36de466ada2cb477e
aelouazz <aymane2030@gmail.com>
2022-10-14 12:41:57 +0100

    Solve Exo 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+	let position = -1;
+	for (const arg of args) {
+		for (const item of arr) {
+			position++;
+			if (arg === item) arr.splice(position, 1);
+		}
+		position = -1;
+	}
 
+	let result = arr;
+	return result;
 };
 
 // Do not edit below this line
