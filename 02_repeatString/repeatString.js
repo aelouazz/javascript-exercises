7624610f169bb44faf96b9f36de01d32c1753362
aelouazz <aymane2030@gmail.com>
2022-10-14 11:59:02 +0100

    Solve Exo 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function (text, count) {
+	if (count < 0) return "ERROR";
+	return text.repeat(count);
 };
 
 // Do not edit below this line
