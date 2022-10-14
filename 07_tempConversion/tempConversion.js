927e3c0688ba17ec29197e67233eeccf27cd7919
aelouazz <aymane2030@gmail.com>
2022-10-14 14:21:34 +0100

    Solve Exo 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (x) {
+	let res = (x - 32) * (5 / 9);
+	return parseFloat(res.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (x) {
+	let res = x * (9 / 5) + 32;
+	return parseFloat(res.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
