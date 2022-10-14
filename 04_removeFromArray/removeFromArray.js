3f273fcadd3133cca833a454e7af6105c086b1ee
aelouazz <aymane2030@gmail.com>
2022-10-14 12:47:41 +0100

    Solve Exo 04_removeFromArray -- Short version

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,22 @@
-const removeFromArray = function (arr, ...args) {
-	let position = -1;
-	for (const arg of args) {
-		for (const item of arr) {
-			position++;
-			if (arg === item) arr.splice(position, 1);
-		}
-		position = -1;
-	}
+var removeFromArray = function(...args) {
+  const array = args[0]
+  return array.filter(val => !args.includes(val))
+}
 
-	let result = arr;
-	return result;
-};
+
+// const removeFromArray = function (arr, ...args) {
+// 	let position = -1;
+// 	for (const arg of args) {
+// 		for (const item of arr) {
+// 			position++;
+// 			if (arg === item) arr.splice(position, 1);
+// 		}
+// 		position = -1;
+// 	}
+
+// 	let result = arr;
+// 	return result;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
