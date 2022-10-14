6df87972c3a7cd6f7628862eacdce1d5df4503a6
aelouazz <aymane2030@gmail.com>
2022-10-14 13:11:45 +0100

    Solve Exo 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumstartll = function (start, end) {
+	if (
+		typeof start != "number" ||
+		typeof end != "number" ||
+		start < 0 ||
+		end < 0
+	)
+		return "ERROR";
+	let i = start < end ? start : end;
+	let j = start < end ? end : start;
+	let sum = 0;
 
+	while (i <= j) {
+		sum += i;
+		i++;
+	}
+	return sum;
 };
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumstartll;
